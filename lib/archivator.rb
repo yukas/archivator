60d3bca93aa4fdf025ee6d7260d45fdc36cad860
Yuri Kasperovich <ykas.gg@gmail.com>
2017-01-17 19:43:50 +0300

    Update archivator.rb

--- lib/archivator.rb
+++ lib/archivator.rb
@@ -1,8 +1,7 @@
-require_relative "decompressor"
 require_relative "compressor"
+require_relative "decompressor"
 
-class Archivator  
-  
+class Archivator
   def initialize
     @letters = []
     @file = ARGV[0]
@@ -17,10 +16,10 @@ class Archivator
   
   private
   
-  attr_reader :letters, :file
+  attr_reader :letters, :file, :compressed_or_decompressed_text
 
   def select_archive_or_unzip    
-    @file = ARGV[1] if ARGV[0] == "-u"         
+    @file = ARGV[1] if ARGV[0] == "-u"
   end
   
   def break_up_the_text_with_the_letters
@@ -29,12 +28,29 @@ class Archivator
     end
   end
   
+  def compress_or_decompress
+    @compressed_or_decompressed_text = 
+      if ARGV[0] != "-u"
+        compressor.creates_archive(letters)
+      else
+        decompressor.shapes_from_archive_file(letters)
+      end
+  end
+  
   def output_to_file
-    if ARGV[0] == "-u"
-      File.open(ARGV[2], "w") {|file| file.write DeCompressor.new.shapes_from_archive_file(letters)}
-    else 
-      File.open(ARGV[1], "w") {|file| file.write Compressor.new.creates_archive(letters)}
+    if ARGV[0] != "-u"
+      File.open(ARGV[1], "w") { |file| compressed_or_decompressed_text) }      
+    else
+      File.open(ARGV[2], "w") { |file| compressed_or_decompressed_text }
     end
+  end
+  
+  def compressor
+    @compressor ||= Compressor.new
+  end
+  
+  def decompressor
+    @compressor ||= Decompressor.new
   end
 end
 
